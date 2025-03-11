57b97798f749ffba2024afe78f1cc07ee27d0dcf
lionX1 <bricancecreative@gmail.com>
2025-03-11 09:19:10 +0100

    deposition

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Appartement</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <!-- home -->
+    <section class="home">
+        <a href="#" class="logo">Chambres</a>
+        <h3>une experience unique</h3>
+        <h1>Zina Hotel</h1>
+        <button class="arrow"><img src="assets/arrow_bottom.png"></button>
+        <button class="btn_style menu_btn">Open Menu</button>
+        <section class="menu">
+
+        </section>
+    </section>
+    <!-- home -->
+
+
+
+
+</body>
+
+</html>
